13a4f528dec3e0f1e95a6a47229aeaa884b13506
hanyusok <hanyusok@gmail.com>
2023-04-01 18:35:33 +0900

    token fetach

--- src/views/CallbackView.vue
+++ src/views/CallbackView.vue
@@ -1,15 +1,11 @@
 <script setup>
+import { async } from '@firebase/util';
 import axios  from 'axios'
 import qs from 'qs'
 import { onMounted, ref } from 'vue'
 
-// const { cookies } = useCookies()
-// let ac = $cookies.get('authorize-access-token')
-// let cookieVal = cookies.get('authorize-access-token')
-console.log(cookieVal)
-//console.log($cookies.get('authorize-access-token'))
-//const authCode = ref(location.search.substring(6))
 const authCode = location.search.substring(6)
+let access_token 
 console.log(`authCode : ${authCode}`)
 const options = {
           'grant_type': 'authorization_code',
@@ -19,44 +15,23 @@ const options = {
           'redirect_uri': 'http://localhost:5173/callback'
         }
 
-
-const getKakaoToken = (authCode) => {   
-  try {
-    axios({
+const getKakaoToken = async() => {     
+    await axios({
         method: 'POST',       
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
           },
-        data: qs.stringify({
-          'grant_type': 'authorization_code',
-         'client_id': '3c4ba9cc89263b9e66bca4c176a4eaf3',
-            // 'client_id': 'a58f889b066015dd555af2bb7577af7e',
-          'client_secret': '8orFiiKOUqaaP5N1fbwfARNMmIuPpJCG',
-          'code': authCode,          
-          'redirect_uri': 'http://127.0.0.1:5173/callback'
-        }),
+        data: qs.stringify(options),
         url: 'https://kauth.kakao.com/oauth/token'
       })
-      .then((response) => {
-          console.log(response)
-      })  
-    } catch(err){
+    .then((response) => {          
+          access_token = response.data.access_token
+          Kakao.Auth.setAccessToken(access_token)       
+      })          
+    .catch((err) => {
       console.log(err)
-    }
-  
-//     //  let token = resp.data.access_token
-//     //  accToken.value = token
-     
-     
-//    //  .then((response) => {
-//    //   console.log(response.data())
-//           // let token = response.data()
-//           // accToken.value = token 
-//           // console.log(token)       
-//           // Kakao.Auth.setAccessToken(ac_token)   
-//      //   })
-   }
-
+    })
+  }
 
 
 </script>
@@ -66,7 +41,10 @@ const getKakaoToken = (authCode) => {
   <div>
     <h1>This is an callback page</h1>
     <p>authCode : {{ authCode }} </p> 
+    <hr>
     <button @click="getKakaoToken">카카오 토큰</button>
+    <hr>
+    <p>access_Token : {{ access_token }} </p> 
     
     
   </div>
