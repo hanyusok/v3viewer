bf70c00dbbb145c730e3bf21c9899ee4192559f7
hanyusok <hanyusok@gmail.com>
2023-04-11 08:41:23 +0900

    router fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,39 +2,48 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DataView from '../views/DataView.vue'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import { useIdentityStore } from '../stores/identityStore'
+import { all } from 'axios'
 
-const router = createRouter({
+
+
+const router = createRouter({  
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      meta: { auth: false},
+      name: 'home',      
       component: HomeView
     },
     {
       path: '/login',
-      name: 'login',
-      meta: { auth: false},
+      name: 'login',      
       component: LoginView
     },  
     {
       path: '/data',
       name: 'data',
-      meta: { auth: true},
-      component: DataView
+      // meta: { auth: true},
+      component: DataView, 
+      beforeEnter: () => {
+        const identityStore = useIdentityStore()
+        if(identityStore.isAllowed){
+          return false
+        } else {
+          return true
+        }
+      }
     }  
   ]
 })
 
 // router.beforeEach((to, from, next) => {
-//   if(to.meta.auth  
-//     ) {
-//       next('/login')
-//     } else if (!to.meta.auth  ) {
+//   const identityStore = useIdentityStore()
+//   const allowed = identityStore.isAllowed
+//   if(to.meta.requiresAuth && allowed) {
 //       next('/data')
 //     } else {
-//       next()
+//       next('/login')
 //     }
 // }) 
 
