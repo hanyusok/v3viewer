bed369af6bb8f167bbfd2d0e23c17f4c01d26f75
hanyusok <hanyusok@gmail.com>
2023-04-11 08:41:34 +0900

    router

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,9 +2,23 @@
 import { ref } from "vue";
 import { NButton, NCard, NInput, NSpace, NRadio, NTag, NSwitch, NIcon } from 'naive-ui'
 import { GlassesOutline, Glasses } from "@vicons/ionicons5";
+import Swal from 'sweetalert2'
+import { useIdentityStore } from '../stores/identityStore'
+// import router from '../router/index'
 
 const loginPassword = "han@6578279"
 const enteredPassword = ref("")
+const identityStore = useIdentityStore()
+const comparePassword = (enteredPassword) => {
+    if (loginPassword === enteredPassword) {
+        identityStore.isAllowed = true
+        Swal.fire(`OK, you're allowed`)
+        // router.go('/data')
+    } else {
+        identityStore.isAllowed = false
+        Swal.fire(`비밀번호 틀립니다!`)
+    }
+}
 
 
 </script>
@@ -14,8 +28,8 @@ const enteredPassword = ref("")
         <n-card title="Staff Only">
             <n-space vertical>
                 <n-input v-model:value="enteredPassword" type="password" show-password-on="mousedown"
-                    placeholder="비밀번호 입력하세요" :maxlength="8" />
-                <n-button tertiary type="success" round block @click="">Login</n-button>
+                    placeholder="비밀번호 입력하세요" :maxlength="11" />
+                <n-button tertiary type="success" round block @click="comparePassword(enteredPassword)">Login</n-button>
             </n-space>
         </n-card>
     </div>
