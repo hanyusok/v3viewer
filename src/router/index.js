85443eec7aad2d3714d3f550c6112a177bb2e0b5
hanyusok <hanyusok@gmail.com>
2023-04-12 10:44:31 +0900

    allmost done

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DataView from '../views/DataView.vue'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
-import { useIdentityStore } from '../stores/identityStore'
-import Swal from 'sweetalert2'
-
-
 
 const router = createRouter({  
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,43 +11,17 @@ const router = createRouter({
       name: 'home',            
       component: HomeView
     },
-    {
-      path: '/login', 
-      name: 'login',      
-      // meta: { auth: false},
-      component: LoginView
-    },  
+    // {
+    //   path: '/login',  
+    //   name: 'login',            
+    //   component: LoginView
+    // },  
     {
       path: '/data',
-      name: 'data',
-      // meta: { auth: true},
-      component: DataView, 
-      // beforeEnter: (to, from, next) => {
-      //   const identityStore = useIdentityStore()
-      //   if (identityStore.isAllowed == false) {
-      //     next('/login')
-      //   } else {
-      //     next()
-      //   }
-      // }
-    
+      name: 'data',      
+      component: DataView,    
     }  
   ]
 })
-
-router.beforeResolve()
-
-
-
-
-router.beforeEach((to, from, next) => {
-  const identityStore = useIdentityStore()
-  if (to.name !== 'login' && !identityStore.isAllowed) next({ name: 'login'})
-  else next()
-})
-
-
-
-
 
 export default router
